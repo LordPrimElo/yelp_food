f61caf069ef7eb0c8e7cd97a5d4877888408ec79
Evan Rathore <evansrathore@gmail.com>
2022-07-04 20:41:31 +0530

    Add landing.js instead of script in HTML

---
+++ public/js/landing.js
@@ -0,0 +1,19 @@
+const body = document.querySelector("body")
+const div = document.getElementById("food-logo-div")
+const img = document.querySelectorAll("img")[1]
+const p = document.getElementById("landing-p")
+const nav = document.querySelector("nav")
+
+img.addEventListener("mouseover", function() {
+	img.setAttribute("src", "/img/FoodLogoWhite.png")
+	body.style.backgroundColor = "black"
+	p.classList.add("landing-p-dark")
+	nav.classList.add("navbar-dark", "bg-dark")
+})
+
+img.addEventListener("mouseleave", function() {
+	img.setAttribute("src", "/img/FoodLogo.png")
+	body.style.backgroundColor = "white"
+	p.classList.remove("landing-p-dark")
+	nav.classList.remove("navbar-dark", "bg-dark")
+})
